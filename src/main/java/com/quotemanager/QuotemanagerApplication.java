37dcd4558c806da025b985593d43cce0223b76f8
Rafael Moura <rafmoura25@outlook.com>
2020-08-20 21:24:15 -0300

    first commit

---
+++ src/main/java/com/quotemanager/QuotemanagerApplication.java
@@ -0,0 +1,13 @@
+package com.quotemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuotemanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuotemanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quotemanager/QuotemanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quotemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuotemanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
